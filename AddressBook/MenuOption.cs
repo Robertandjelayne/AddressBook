98611b16ac75fa1d5d86092ecd7780da47b4c54a
Robertandjelayne <robert.mills@bcbsnc.com>
2017-04-25 12:15:26 -0400

    added Enum

---
+++ AddressBook/MenuOption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public enum MenuOption
+    {
+        Exit,
+        AddPerson,
+        AddCompany,
+        ListContacts,
+        SearchContacts,
+        RemoveContact
+        
+    }
+}
